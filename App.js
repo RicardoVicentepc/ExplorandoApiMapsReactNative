4f09e7806f41f58a8dd95eb6f21899284c50c187
Ricardo Vicente <86174349+RicardoVicentepc@users.noreply.github.com>
2022-10-31 23:33:36 -0300

    Coloquei a misera do icone O COMEÇO DE TUDO ;-;

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
 
 export default function App() {
+const casaDaJulia = 
+{
+  latitude:-23.5617769,
+  longitude:-46.3989296,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+}  
+const meuBarraco =
+{
+  latitude:-23.5272941,
+  longitude:-46.4171535,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}>
+      <Marker coordinate={casaDaJulia}>
+          <View style={{backgroundColor: "red", padding: 10, borderRadius:100}}>
+          <Text>id1</Text>
+        </View>
+      </Marker>
+      <Marker coordinate={meuBarraco}>
+          <View style={{backgroundColor: "red", padding: 10, borderRadius:100}}>
+          <Text>id1</Text>
+        </View>
+      </Marker>
+      </MapView>
     </View>
   );
 }
@@ -16,5 +42,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
