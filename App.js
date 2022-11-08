01b0737acf94769f7584d107463c92bf5c4dc829
Ricardo Vicente <86174349+RicardoVicentepc@users.noreply.github.com>
2022-11-07 23:05:53 -0300

    CUSTOMMARKER CONDENADO

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { useEffect, useState } from "react";
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
-
+import { StyleSheet, Text, View, Dimensions, Image} from 'react-native';
+import CustomMarker from 'CustomMarker';
+import Geolocation from 'react-native-geolocation-service';
+import * as Location from 'expo-location';
 
 export default function App() {
 const casaDaJulia = 
@@ -11,26 +14,78 @@ const casaDaJulia =
   latitudeDelta: 0.01,
   longitudeDelta: 0.01,
 }  
-const meuBarraco =
-{
-  latitude:-23.5272941,
-  longitude:-46.4171535,
-  latitudeDelta: 0.01,
-  longitudeDelta: 0.01,
-}
+const [camera, setCamera] = useState({
+  center:{
+     latitude: 0,
+     longitude: 0
+  },
+  pitch: 0,
+  heading: 0,
+  altitude: 1000,
+  zoom: 16,
+});
+useEffect(() => {
+  const startTracking = async () => {
+     let { status } = await requestForegroundPermissionsAsync();
+     //Obtendo permissões de localização, é necesário que as permissões sejam concedidas para que possamos obter a localização do usuário         
+     if (status !== 'granted') {
+        alert('Permissões para acessar a localização foram negadas.');
+        return;
+     }
+     try {
+         /* A função abaixo realiza o monitoramento da posição atual do usuário de acordo com os parâmetros fornecidos
+           e retorna uma callback sempre que obtém a localização, a partir da callback iremos obter um objeto contendo as coordenadas */
+        await watchPositionAsync({
+           accuracy: Accuracy.Highest,
+           timeInterval: 5000,
+           distanceInterval: 50,
+        }, (loc) => {
+           /*
+              Setando o estado da câmera a partir do operador spread, pois desejamos manter as demais propriedades da câmera intactas,
+              senão o utilizarmos o spread precisaremos definir as demais propriedas novamente, 
+              fugindo do nosso objetivo de criar uma câmera dinâmica
+           */
+           setCamera( prevCamera => ({
+              ...prevCamera,
+              center: {
+                 latitude: loc.coords.latitude,
+                 longitude: loc.coords.longitude,
+              }
+           }));
+        }
+        );
+     } catch (err) {
+        console.warn('Algo deu errado...');
+     }
+  }
+  startTracking();
+}, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}>
-      <Marker coordinate={casaDaJulia}>
-          <Image source={require('./assets/icon.png')} style={{width:80,height:10}}/>
-           <View style={{backgroundColor: "purple"}}>
-        </View>
-      </Marker>
-      <Marker coordinate={meuBarraco}>
-          <View style={{backgroundColor: "red", padding: 10, borderRadius:100}}>
-          <Text>id1</Text>
-        </View>
-      </Marker>
+   <MapView
+         style={styles.map}
+         camera={camera}
+         showsUserLocation={true}
+         showsMyLocationButton={false}
+         zoomControlEnabled={true}
+         loadingEnabled={true}
+         loadingBackgroundColor={'#fff'}
+         toolbarEnabled={false}
+      >
+         <Marker
+            latitude={-23.5544}
+            longitude={-46.6296}
+            color={"#0F9D58"}
+            id={'1'}
+         >
+         </Marker>
+         <Marker
+            latitude={-23.5583}
+            longitude={-46.6282}
+            id={'2'}
+         >
+         </Marker>
       </MapView>
     </View>
   );
